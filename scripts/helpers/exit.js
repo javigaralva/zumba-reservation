4b335e2bb08d999924899fb6f2c77ee651dab886
javigaralva <javigaralva@gmail.com>
2022-11-14 18:02:01 +0100

    feat: add error to print

--- scripts/helpers/exit.js
+++ scripts/helpers/exit.js
@@ -1,23 +1,37 @@
+import { inspect } from 'node:util'
 import notifyUsers from './notifyUsers.js'
 import takeScreenshotAndNotifyUsers from './takeScreenshotAndNotifyUsers.js'
 
-export async function exitWithError({ page, text, takeScreenShot = true, notify = true } = {}) {
-    return await exit({ page, text, takeScreenShot, exitCode: -1, notify })
+export async function exitWithError({ page, error, text, takeScreenShot = true, notify = true } = {}) {
+    return await exit({ page, error, text, takeScreenShot, exitCode: -1, notify })
 }
 
 export async function exitOk({ page, text, takeScreenShot = false, notify = false } = {}) {
     return await exit({ page, text, takeScreenShot, exitCode: 0, notify })
 }
 
-async function exit({ page, text, exitCode, takeScreenShot, notify} = {}) {
+async function exit({ page, error, text, exitCode, takeScreenShot, notify} = {}) {
     if (text !== undefined) {
-        exitCode === -1 ? console.error(text) : console.log(text)
+        exitCode === -1
+            ? console.error(text)
+            : console.log(text)
+    }
+
+    const stringifiedError = (error && inspect(error, { showHidden: true })) ?? ''
+    if (stringifiedError) {
+        exitCode === -1
+            ? console.error(stringifiedError)
+            : console.log(stringifiedError)
     }
 
     if (notify) {
         takeScreenShot
             ? await takeScreenshotAndNotifyUsers({ page, text })
             : await notifyUsers({ text })
+
+        if (stringifiedError) {
+            await notifyUsers({ text: stringifiedError })
+        }
     }
 
     process.exit(exitCode)
