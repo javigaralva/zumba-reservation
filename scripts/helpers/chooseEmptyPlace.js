f5ff34661a101b9f92e03bb4f8fa8151ce991ce5
javigaralva <javigaralva@gmail.com>
2023-01-24 00:04:41 +0100

    Change reservation order

--- scripts/helpers/chooseEmptyPlace.js
+++ scripts/helpers/chooseEmptyPlace.js
@@ -3,7 +3,7 @@ import { exitWithError } from './exit.js'
 export default async function chooseEmptyPlace({ page }) {
     console.log('Eligiendo un sitio libre...')
     const allPlacesNumbers = Array.from({ length: 50 }, (_, i) => String(i + 1))
-    const preferencePlacesOrder = ['40', '50', '39', '29', '49', '48', '47', '46', '45', '44', '43', '42', '41', '38', '37', '35', '29', '28', '34', '33', '31', '30', '21', '27', '20', '19', '14', '13', '12', '11', '10', '1', '2', '26', '25', '24', '17', '16', '15', '3', '4', '5', '6', '7', '8', '9']
+    const preferencePlacesOrder = ['50', '40', '39', '29', '49', '48', '47', '46', '45', '44', '43', '42', '41', '38', '37', '35', '29', '28', '34', '33', '31', '30', '21', '27', '20', '19', '14', '13', '12', '11', '10', '1', '2', '26', '25', '24', '17', '16', '15', '3', '4', '5', '6', '7', '8', '9']
     const placesOrder = [...new Set([...preferencePlacesOrder, ...allPlacesNumbers])]
 
     let placeFound
