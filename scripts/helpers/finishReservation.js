4f5f892f8b8828328ebb52c4ac827c794b6dc040
javigaralva <javigaralva@gmail.com>
2023-03-15 21:30:29 +0100

    feat: wait less time

--- scripts/helpers/finishReservation.js
+++ scripts/helpers/finishReservation.js
@@ -1,17 +1,26 @@
 import { exitWithError } from './exit.js'
 
+const FINISH_RESERVATION_ERRORS = {
+    INVALID_RESERVATION_TIME_1_DAY: 'INVALID_RESERVATION_TIME_1_DAY'
+}
+
 export default async function finishReservation({ page }) {
     console.log('Reservando plaza de zumba...')
 
     await page.getByText('Reservar').nth(1).click()
-
-    await page.waitForTimeout(10000)
+    await page.waitForTimeout(5000)
+    await page.waitForLoadState('networkidle')
 
     {
         const errorLocator = await page.locator('.box-datos-error')
         const isErrorLocatorVisible = await errorLocator.isVisible()
         if (isErrorLocatorVisible) {
-            await exitWithError({ page, text: 'No se puede realizar la reserva: ' + (await errorLocator.innerText()) })
+            const errorText = await errorLocator.innerText()
+            if (errorText.toUpperCase().includes('NO SE PUEDEN REALIZAR RESERVAS CON UNA ANTELACIÓN SUPERIOR A 1 DÍA')) {
+                console.log(errorText)
+                return FINISH_RESERVATION_ERRORS.INVALID_RESERVATION_TIME_1_DAY
+            }
+            await exitWithError({ page, text: 'No se puede realizar la reserva: ' + errorText })
         }
     }
     {
