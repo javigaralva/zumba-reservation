2cdf0bf5e86d67c4bfdb524422767f0c9b9175c1
Javier García Álvarez <javier.garcia6@decathlon.com>
2023-11-18 13:51:52 +0100

    fix: login using Enter key

--- scripts/helpers/doLogin.js
+++ scripts/helpers/doLogin.js
@@ -6,17 +6,18 @@ export default async function doLogin({ browser, page, user, password, url, disp
     await page.goto(url)
 
     await page.getByLabel('Usuario').click()
-
     await page.getByLabel('Usuario').fill(user)
-
     await page.getByLabel('Usuario').press('Tab')
 
-    await page.locator('input[name="password"]').fill(password)
+    await page.getByLabel('Contraseña').fill(password)
+    await page.getByLabel('Contraseña').press('Enter')
 
-    await page.getByRole('button', { name: 'Acceder' }).click()
+    // await page.getByRole('button', { name: 'Acceder' }).click()
 
-    const isDisplayedNameVisible = await page.getByRole('link', { name: displayedName }).isVisible()
-    if (!isDisplayedNameVisible) {
-        await exitWithError({ browser, page, text: 'No se ha realizado login correctamente. No se ha encontrado el nombre del usuario en el profile.' })
-    }
+    await page.getByRole('link', { name: displayedName }).waitFor({ state: "visible"})
+    
+    // const isDisplayedNameVisible = await link.isVisible()
+    // if (!isDisplayedNameVisible) {
+    //     await exitWithError({ browser, page, text: 'No se ha realizado login correctamente. No se ha encontrado el nombre del usuario en el profile.' })
+    // }
 }
